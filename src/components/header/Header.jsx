37a52f64a4af2c61039721ef27041b598dad7a81
Erislandio <erislandio.soares@acct.global>
2019-09-17 23:43:30 -0300

    add topbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,221 @@
 import React from "react";
-import './header.css'
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import PeopleIcon from "@material-ui/icons/People";
+import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
+import PictureInPictureAltIcon from "@material-ui/icons/PictureInPictureAlt";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import AccountBalanceWalletIcon from "@material-ui/icons/AccountBalanceWallet";
+import LaptopChromebookIcon from "@material-ui/icons/LaptopChromebook";
 
-export default class Header extends React.Component {
-  render() {
-    return <header>header</header>;
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    background: "#2c3e50"
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
   }
+}));
+
+const menuItems = [
+  {
+    id: 1,
+    name: "Console",
+    icon: <PictureInPictureAltIcon />,
+    link: "/console"
+  },
+  {
+    id: 2,
+    name: "Departamentos",
+    icon: <SportsEsportsIcon />,
+    link: "/departament"
+  },
+  {
+    id: 3,
+    name: "Categorias",
+    icon: <AccountBalanceWalletIcon />,
+    link: "/categories"
+  },
+  {
+    id: 4,
+    name: "Produto",
+    icon: <SportsEsportsIcon />,
+    link: "/products"
+  }
+];
+
+const otherLinks = [
+  {
+    id: 1,
+    name: "Front",
+    icon: <LaptopChromebookIcon />,
+    link: "/frontend"
+  },
+  {
+    id: 2,
+    name: "Membros",
+    icon: <PeopleIcon />,
+    link: "/frontend"
+  },
+  {
+    id: 3,
+    name: "Sair",
+    icon: <PowerSettingsNewIcon />,
+    link: "/login"
+  }
+];
+
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  function handleDrawerOpen() {
+    setOpen(true);
+  }
+
+  function handleDrawerClose() {
+    setOpen(false);
+  }
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {menuItems.map((item, index) => (
+            <ListItem button key={item.id}>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {otherLinks.map((item, index) => (
+            <ListItem button key={item.id}>
+              <ListItemIcon>
+                  {
+                      item.icon
+                  }
+              </ListItemIcon>
+              <ListItemText primary={item.name} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+    </div>
+  );
 }
